145c513aa9f933b98a25c933e10e424801a6312d
huyanhit <xulanh00@gmail.com>
2016-07-12 16:31:31 +0700

    up

--- wordpress/wp-content/themes/semitri/vc_params/zo_fancybox_single.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$params = array(
-        array(
-            "type" => "textfield",
-            "heading" => __("Title item 2",'semitri'),
-            "param_name" => "zo_fancybox_title",
-        ),
-	);
